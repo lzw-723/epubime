f54c0ea8233901c5e99baa3e23df6c1101e37041
lzw-723 <lzw-723@qq.com>
2023-06-06 17:33:27 +0800

    test: 修复ListUtilsTest

--- src/test/java/fun/lzwi/epubime/util/ListUtilsTest.java
+++ src/test/java/fun/lzwi/epubime/util/ListUtilsTest.java
@@ -2,6 +2,7 @@ package fun.lzwi.epubime.util;
 
 import static org.junit.Assert.assertTrue;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Test;
@@ -9,7 +10,7 @@ import org.junit.Test;
 public class ListUtilsTest {
     @Test
     public void testCopy() {
-        List<String> list = List.of(new String[] { "a", "b" });
+        List<String> list = Arrays.asList(new String[] { "a", "b" });
         List<String> copy = ListUtils.copy(list);
         assertTrue(list.containsAll(copy));
     }
