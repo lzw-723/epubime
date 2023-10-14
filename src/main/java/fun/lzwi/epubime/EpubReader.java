84c9a67a2daeeabdf908e9ffb733e32b8abda967
lzw-723 <lzw-723@qq.com>
2023-10-14 19:49:43 +0800

    fix: 修复opf文件在zip根目录造成的异常
    添加系统测试，测试用例存放在src\test\resources\epub，不公开

--- src/main/java/fun/lzwi/epubime/EpubReader.java
+++ src/main/java/fun/lzwi/epubime/EpubReader.java
@@ -32,14 +32,16 @@ public class EpubReader {
         Path path = Paths.get(opf).getParent();
         String parent = "";
         if (path != null) {
-            parent = path.toString();
+            parent = path.toString() + "/";
         }
         PackageDocument packageDocument = new PackageDocumentReader(file.getInputStream(opf)).read();
         epub.setPackageDocument(packageDocument);
         String version = packageDocument.getVersion();
         if (EpubConstants.EPUB_VERSION_3.equals(version)) {
-            ManifestItem nav = packageDocument.getManifest().getItems().stream().filter(i -> "nav".equals(i.getProperties())).findFirst().get();
-            NavigationDocReader navigationDocReader = new NavigationDocReader(file.getInputStream(parent + "/" + nav.getHref()));
+            ManifestItem nav = packageDocument.getManifest().getItems().stream()
+                    .filter(i -> "nav".equals(i.getProperties())).findFirst().get();
+            NavigationDocReader navigationDocReader = new NavigationDocReader(
+                    file.getInputStream(parent + nav.getHref()));
             NavigationDocument navigationDocument;
             navigationDocument = navigationDocReader.read();
             epub.setNavigationDocument(navigationDocument);
@@ -47,8 +49,9 @@ public class EpubReader {
             // ncx文件要求
             // Key NCX Requirements
             // https://idpf.org/epub/20/spec/OPF_2.0.1_draft.htm#TOC2.4.1.2
-            ManifestItem ncx = packageDocument.getManifest().getItems().stream().filter(i -> EpubConstants.EPUB_2_NCX_MediaType.equals(i.getMediaType())).findFirst().get();
-            NCXReader reader = new NCXReader(file.getInputStream(parent + "/" + ncx.getHref()));
+            ManifestItem ncx = packageDocument.getManifest().getItems().stream()
+                    .filter(i -> EpubConstants.EPUB_2_NCX_MediaType.equals(i.getMediaType())).findFirst().get();
+            NCXReader reader = new NCXReader(file.getInputStream(parent + ncx.getHref()));
             NCX nav = reader.read();
             epub.setNCX(nav);
         }
---
+++ src/test/java/fun/lzwi/epubime/SystemTest.java
@@ -0,0 +1,33 @@
+package fun.lzwi.epubime;
+
+import static org.junit.Assert.assertNotNull;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.junit.Test;
+import org.xml.sax.SAXException;
+
+import fun.lzwi.Utils;
+
+public class SystemTest {
+    @Test
+    public void testEpubs() throws IOException, ParserConfigurationException, SAXException {
+        // src\test\resources\epub
+        // 此目录存放收集自互联网的epub文件，可能涉及版权问题，故不上传
+        File dir = Utils.getFile("epub");
+        for (File file : dir.listFiles()) {
+            if (file.getName().endsWith(".epub")) {
+                testBook(file);
+            }
+        }
+    }
+
+    public void testBook(File file) throws IOException, ParserConfigurationException, SAXException {
+        EpubReader reader = new EpubReader(new EpubFile(file));
+        Epub epub = reader.read();
+        assertNotNull(String.format("读取：%s失败", file.getName()), epub);
+    }
+}
